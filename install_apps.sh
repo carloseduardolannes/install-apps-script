b400b54a84ffb9ba4cd76745b93dc72476be323a
carloseduardolannes <85688001+carloseduardolannes@users.noreply.github.com>
2024-09-15 17:15:35 -0300

    Atualizar o install_apps.sh

--- install_apps.sh
+++ install_apps.sh
@@ -1,82 +1,129 @@
 #!/bin/bash
 
-# Verifica se o script está sendo executado como root (UID 0 é do root)
+# Verifica se o script está sendo executado como root
 if [ "$EUID" -ne 0 ]; then 
   echo "Por favor, execute como root"
   exit
 fi
 
 # Atualiza a lista de pacotes
-echo "Atualizando lista de pacotes..."
 sudo apt update
 
-# Adiciona o repositório oficial do Visual Studio Code
-echo "Adicionando repositório do Visual Studio Code..."
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
+# Função para criar atalhos no menu principal
+create_desktop_entry() {
+    local name=$1
+    local exec=$2
+    local icon=$3
+    local category=$4
 
-# Adiciona o repositório do GeoGebra
-echo "Adicionando repositório do GeoGebra..."
-sudo sh -c 'echo "deb http://www.geogebra.net/linux/ stable main" > /etc/apt/sources.list.d/geogebra.list'
-wget -qO- https://static.geogebra.org/linux/office@geogebra.org.gpg.key | sudo apt-key add -
-
-# Atualiza a lista de pacotes novamente após adicionar os novos repositórios
-sudo apt update
-
-# Lista de aplicativos para instalar
-apps=(
-    "git"                  # Ferramenta de controle de versão
-    "curl"                 # Ferramenta para transferir dados de ou para um servidor
-    "vim"                  # Editor de texto poderoso e amplamente utilizado
-    "htop"                 # Monitor de processos interativo no terminal
-    "gimp"                 # Editor de imagens avançado
-    "vlc"                  # Reprodutor de mídia versátil
-    "docker.io"            # Plataforma para automação de aplicativos usando containers
-    "python3-pip"          # Gerenciador de pacotes para Python
-    "code"                 # Visual Studio Code, editor de código poderoso
-    "geogebra-classic"      # Aplicativo de matemática dinâmico, GeoGebra
-    "anki"                 # Ferramenta de flashcards para auxiliar na memorização
-    "artha"                # Tesauro offline baseado no WordNet
-    "bibletime"            # Aplicativo de estudo bíblico
-    "celestia"             # Simulação espacial 3D
-    "chemtool"             # Programa para desenhar estruturas químicas
-    "epoptes"              # Ferramenta de monitoramento de laboratórios de informática
-    "gap"                  # Sistema de álgebra computacional
-    "gcompris"             # Software educacional para crianças
-    "gnome-dictionary"     # Dicionário para o ambiente GNOME
-    "octave"               # Linguagem de programação científica compatível com MATLAB
-    "gtypist"              # Instrutor de digitação em modo texto
-    "gnukhata"             # Software de contabilidade
-    "gperiodic"            # Tabela periódica para Linux
-    "klavaro"              # Tutor de digitação
-    "ktouch"               # Programa para aprender e praticar digitação
-    "marble"               # Globo virtual e atlas mundial
-    "maxima"               # Software de manipulação de expressões simbólicas e numéricas
-    "stellarium"           # Planetário open-source
-    "ugene"                # Software de bioinformática com GUI
-    "veyon"                # Gerenciamento de computadores em sala de aula
-)
+    # Criando o arquivo .desktop
+    echo "Criando atalho para $name..."
+    echo "[Desktop Entry]
+Name=$name
+Exec=$exec
+Icon=$icon
+Type=Application
+Categories=$category;
+" | sudo tee /usr/share/applications/${name,,}.desktop > /dev/null
+}
 
 # Instala os aplicativos
-echo "Instalando aplicativos..."
-for app in "${apps[@]}"; do
-    echo "Instalando $app..."
-    sudo apt install -y $app
-done
+install_apps() {
+    local category=("$@")
+    for app in "${category[@]}"; do
+        echo "Instalando $app..."
+        sudo apt install -y $app
+    done
+}
 
-# Aplicativos adicionais que exigem instalações manuais
-echo "Instalando aplicativos adicionais..."
+# Ferramentas de Desenvolvimento
+dev_tools=(
+    "git"
+    "curl"
+    "vim"
+    "docker.io"
+    "python3-pip"
+    "code"
+)
 
-# Google Earth
+# Ferramentas Educacionais
+edu_tools=(
+    "geogebra-classic"
+    "anki"
+    "gcompris"
+    "maxima"
+    "stellarium"
+    "ktouch"
+    "klavaro"
+)
+
+# Aplicativos Científicos
+science_tools=(
+    "octave"
+    "celestia"
+    "chemtool"
+    "gperiodic"
+    "gap"
+    "ugene"
+)
+
+# Ferramentas de Estudo e Pesquisa
+study_tools=(
+    "bibletime"
+    "artha"
+    "gnome-dictionary"
+)
+
+# Gerenciamento de Sistemas
+sys_tools=(
+    "htop"
+    "veyon"
+    "gnukhata"
+    "epoptes"
+)
+
+# Instalação de Ferramentas de Desenvolvimento
+echo "Instalando Ferramentas de Desenvolvimento..."
+install_apps "${dev_tools[@]}"
+create_desktop_entry "Git" "/usr/bin/git" "git" "Development"
+create_desktop_entry "Curl" "/usr/bin/curl" "utilities-terminal" "Development"
+create_desktop_entry "Visual Studio Code" "/usr/bin/code" "code" "Development"
+
+# Instalação de Ferramentas Educacionais
+echo "Instalando Ferramentas Educacionais..."
+install_apps "${edu_tools[@]}"
+create_desktop_entry "GeoGebra" "/usr/bin/geogebra" "geogebra" "Education"
+create_desktop_entry "Anki" "/usr/bin/anki" "anki" "Education"
+create_desktop_entry "GCompris" "/usr/bin/gcompris" "gcompris" "Education"
+
+# Instalação de Aplicativos Científicos
+echo "Instalando Aplicativos Científicos..."
+install_apps "${science_tools[@]}"
+create_desktop_entry "Octave" "/usr/bin/octave" "octave" "Science"
+create_desktop_entry "Celestia" "/usr/bin/celestia" "celestia" "Science"
+create_desktop_entry "Chemtool" "/usr/bin/chemtool" "chemtool" "Science"
+
+# Instalação de Ferramentas de Estudo e Pesquisa
+echo "Instalando Ferramentas de Estudo e Pesquisa..."
+install_apps "${study_tools[@]}"
+create_desktop_entry "BibleTime" "/usr/bin/bibletime" "bibletime" "Education"
+create_desktop_entry "Artha" "/usr/bin/artha" "artha" "Education"
+create_desktop_entry "Gnome Dictionary" "/usr/bin/gnome-dictionary" "accessories-dictionary" "Utility"
+
+# Instalação de Ferramentas de Gerenciamento de Sistemas
+echo "Instalando Ferramentas de Gerenciamento de Sistemas..."
+install_apps "${sys_tools[@]}"
+create_desktop_entry "Htop" "/usr/bin/htop" "utilities-system-monitor" "System"
+create_desktop_entry "Veyon" "/usr/bin/veyon" "veyon" "System"
+
+# Instalação de aplicativos adicionais
 echo "Instalando Google Earth..."
 wget -q https://dl.google.com/dl/earth/client/current/google-earth-pro-stable_current_amd64.deb
 sudo apt install -y ./google-earth-pro-stable_current_amd64.deb
 rm google-earth-pro-stable_current_amd64.deb
+create_desktop_entry "Google Earth" "/usr/bin/google-earth-pro" "google-earth" "Science"
 
 # Limpeza após a instalação
-echo "Limpando pacotes desnecessários..."
 sudo apt autoremove -y
 
 echo "Instalação concluída!"
