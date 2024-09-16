e124d4ca2ddfe41ddd6f9355b4b8f253f8bc1495
carloseduardolannes <85688001+carloseduardolannes@users.noreply.github.com>
2024-09-16 09:51:04 -0300

    Update install_apps.sh

--- install_apps.sh
+++ install_apps.sh
@@ -44,17 +44,30 @@ dev_tools=(
     "docker.io"
     "python3-pip"
     "code"
+    "arduino"
+    "netbeans"
+    "pycharm-community"
+    "jupyter-notebook"
 )
 
 # Ferramentas Educacionais
 edu_tools=(
+    "gcompris"
     "geogebra-classic"
     "anki"
-    "gcompris"
     "maxima"
     "stellarium"
     "ktouch"
     "klavaro"
+    "scratch"
+    "kturtle"
+    "kgeography"
+    "minuet"
+    "blinken"
+    "kstars"
+    "ktechlab"
+    "step"
+    "marble"
 )
 
 # Aplicativos Científicos
@@ -72,6 +85,13 @@ study_tools=(
     "bibletime"
     "artha"
     "gnome-dictionary"
+    "convertall"
+)
+
+# Ferramentas de Utilidade e Outros
+utility_tools=(
+    "kmag"
+    "sweet-home3d"
 )
 
 # Gerenciamento de Sistemas
@@ -79,7 +99,6 @@ sys_tools=(
     "htop"
     "veyon"
     "gnukhata"
-    "epoptes"
 )
 
 # Instalação de Ferramentas de Desenvolvimento
@@ -88,13 +107,25 @@ install_apps "${dev_tools[@]}"
 create_desktop_entry "Git" "/usr/bin/git" "git" "Development"
 create_desktop_entry "Curl" "/usr/bin/curl" "utilities-terminal" "Development"
 create_desktop_entry "Visual Studio Code" "/usr/bin/code" "code" "Development"
+create_desktop_entry "Arduino IDE" "/usr/bin/arduino" "arduino" "Development"
+create_desktop_entry "Netbeans" "/usr/bin/netbeans" "netbeans" "Development"
+create_desktop_entry "PyCharm" "/usr/bin/pycharm-community" "pycharm" "Development"
+create_desktop_entry "Jupyter Notebook" "/usr/bin/jupyter-notebook" "jupyter" "Development"
 
 # Instalação de Ferramentas Educacionais
 echo "Instalando Ferramentas Educacionais..."
 install_apps "${edu_tools[@]}"
+create_desktop_entry "GCompris" "/usr/bin/gcompris" "gcompris" "Education"
 create_desktop_entry "GeoGebra" "/usr/bin/geogebra" "geogebra" "Education"
 create_desktop_entry "Anki" "/usr/bin/anki" "anki" "Education"
-create_desktop_entry "GCompris" "/usr/bin/gcompris" "gcompris" "Education"
+create_desktop_entry "KTurtle" "/usr/bin/kturtle" "kturtle" "Education"
+create_desktop_entry "KGeography" "/usr/bin/kgeography" "kgeography" "Education"
+create_desktop_entry "Minuet" "/usr/bin/minuet" "minuet" "Education"
+create_desktop_entry "Blinken" "/usr/bin/blinken" "blinken" "Education"
+create_desktop_entry "KStars" "/usr/bin/kstars" "kstars" "Science"
+create_desktop_entry "KTechlab" "/usr/bin/ktechlab" "ktechlab" "Education"
+create_desktop_entry "Step" "/usr/bin/step" "step" "Science"
+create_desktop_entry "Marble" "/usr/bin/marble" "marble" "Education"
 
 # Instalação de Aplicativos Científicos
 echo "Instalando Aplicativos Científicos..."
@@ -109,6 +140,13 @@ install_apps "${study_tools[@]}"
 create_desktop_entry "BibleTime" "/usr/bin/bibletime" "bibletime" "Education"
 create_desktop_entry "Artha" "/usr/bin/artha" "artha" "Education"
 create_desktop_entry "Gnome Dictionary" "/usr/bin/gnome-dictionary" "accessories-dictionary" "Utility"
+create_desktop_entry "Convertall" "/usr/bin/convertall" "convertall" "Utility"
+
+# Instalação de Ferramentas de Utilidade e Outros
+echo "Instalando Ferramentas de Utilidade..."
+install_apps "${utility_tools[@]}"
+create_desktop_entry "KMag" "/usr/bin/kmag" "kmag" "Utility"
+create_desktop_entry "Sweet Home 3D" "/usr/bin/sweethome3d" "sweethome3d" "Graphics"
 
 # Instalação de Ferramentas de Gerenciamento de Sistemas
 echo "Instalando Ferramentas de Gerenciamento de Sistemas..."
